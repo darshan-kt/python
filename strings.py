5533ca078f4b47e271ff45bbeca5a461a5cc8711
darshan-kt <54614620+darshan-kt@users.noreply.github.com>
2019-10-26 01:42:30 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -3,10 +3,10 @@
 my_string = 'abcdefg'
 print(mystring[1])    //b
 print(mystring[-1])    //g
-print(mystring[1:])    //bbdcefg
-print(mystring[:3])    //abc
-print(mystring[1:4])    //bcd
-print(mystring[1:6:2])    //bdfg
-print(mystring[::1])    //abcdefg   -->one of increment
-print(mystring[::2])    //aceg      -->two increment
-print(mystring[::-1])    //gfedcba   Note:Interview question: Reverse the string or list whithout using for loop
+print(mystring[1:])    //bbdcefg       from 1 onwards
+print(mystring[:3])    //abc           upto 3
+print(mystring[1:4])    //bcd           from 1 to 4
+print(mystring[1:6:2])    //bdfg        from 1 to 6 with 2 increment
+print(mystring[::1])    //abcdefg     one of increment
+print(mystring[::2])    //aceg        two increment
+print(mystring[::-1])    //gfedcba    Note:Interview question: Reverse the string or list whithout using for loop
