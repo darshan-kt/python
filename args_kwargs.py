3de65b85b2c9aba1e943b048ff8c51f7627668fb
darshan-kt <54614620+darshan-kt@users.noreply.github.com>
2020-04-02 19:52:55 +0530

    Update args_kwargs.py

--- args_kwargs.py
+++ args_kwargs.py
@@ -7,7 +7,7 @@ def my_fun(a,b):
 '''
 def my_fun(*args):
     return(sum(args))
-print(my_fun(1,2,3,4,5))
+print(my_fun(1,2,3,4,5))   //15
 '''
 
 #  **kwargs are another kind of data structure, stores n number of dictionary inside it and return back Dictionarys
@@ -16,12 +16,12 @@ def my_fun(**kwargs):
     print(kwargs)
     if 'fruit' in kwargs:
         print("My fruit of choice is {}". format(kwargs['fruit']))
-print(my_fun(fruit='apple'))
+print(my_fun(fruit='apple'))        //My fruit of choice is apple
 '''
 
 def myfun(*args, **kwargs):
-    print(args)
-    print(kwargs)
+    print(args)                                       //(10,20,30)
+    print(kwargs)                                     //{'food': 'egg rice', 'fruit': 'banana'}
     print("I will buy {} {}" .format(args[0], kwargs['food']))
     
-print(myfun(10,20,30, fruit='banana', food='egg rice'))
+print(myfun(10,20,30, fruit='banana', food='egg rice'))   //I will buy 10 egg rice
