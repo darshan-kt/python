85a012a22d253408706ff51a803891fded990e3a
darshan-kt <54614620+darshan-kt@users.noreply.github.com>
2020-07-16 16:06:34 +0530

    Update Function.py

--- Function.py
+++ Function.py
@@ -1,7 +1,7 @@
 def reverse(text):
-    wordlist = text.split()
-    reverse_list = wordlist[::-1]
-    print(' '.join(reverse_list))
+    wordlist = text.split()    #split() breaks the each words seperate in list
+    reverse_list = wordlist[::-1]  # this will reverse reverse the words order in list
+    print(' '.join(reverse_list))   # ''.join() function joins the string from list and element list gives string
     return wordlist
 
 reverse('I am boss')
